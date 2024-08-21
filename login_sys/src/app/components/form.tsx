cc38c92e1596e5cdc043b1a31060f3c1a4706415
kmjak <tymkoron1221@icloud.com>
2024-08-21 15:52:49 +0900

    完成

--- login_sys/src/app/components/form.tsx
+++ login_sys/src/app/components/form.tsx
@@ -6,10 +6,10 @@ import {v4 as uuidv4} from "uuid"
 import { useRouter } from 'next/navigation';
 
 interface FormProps {
-    isRegister:boolean;
+    mode:string
 }
 
-export const Form = ({isRegister}:FormProps)=>{
+export const Form = ({mode}:FormProps)=>{
     const [NAME,setName] = useState("");
     const [PASS,setPass] = useState("");
     const router = useRouter();
@@ -20,7 +20,7 @@ export const Form = ({isRegister}:FormProps)=>{
         }
         const users = await getAllUsers();
         const isExist = users.find((user)=> user.name === NAME);
-        if(isRegister) {
+        if(mode == "signup") {
             if(isExist){
                 return alert("User already exist")
             }
@@ -29,7 +29,7 @@ export const Form = ({isRegister}:FormProps)=>{
             setPass("");
             return alert("User registered")
         }
-        if(!isRegister) {
+        if(mode == "login") {
             if(!isExist){
                 return alert("Failed to login")
             }
